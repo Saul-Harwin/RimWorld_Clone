cd36a5fdf54d4c991e3edf9f65abf7631e315be2
ErikBolumburu <erikbolumburu11@gmail.com>
2022-07-03 23:42:59 +0100

    Update ObjectPlacer.cs

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ObjectPlacer : MonoBehaviour {
     public float radius = 1.41421356237f;
-    public int width = 100;
-    public int height = 100;
+    int width = 100;
+    int height = 100;
     public int numSampleBeforeRejection = 30;
 
     List<Vector2> points;
 
     public void PlaceObject() {
+        width = GameManager.Instance.world.width;
+        height = GameManager.Instance.world.height;
         points = PoissonDiscSampling.Place(width, height, radius, numSampleBeforeRejection);
         Tile[,] tiles = this.GetComponent<World>().tiles;
         foreach (Vector2 point in points) {
