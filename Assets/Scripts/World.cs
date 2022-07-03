757451ad26e5f3859c157354d1ef15e29ea71ea6
SaulHarwin <saulharwin@gmail.com>
2022-07-03 20:21:30 +0100

    Object Placement

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class World : MonoBehaviour {
     public TileData[] tileHeights;
-
     public int width, height;
     public Tile[,] tiles;
     // [SerializeField] Tile grassTile, waterTile;
@@ -23,11 +22,12 @@ public class World : MonoBehaviour {
         tiles = new Tile[width, height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                tiles[x,y] = PickTile(heightMap[x,y]);
+                tiles[x,y] = ScriptableObject.Instantiate(PickTile(heightMap[x,y]));
                 bool isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 tiles[x,y].Initialize(new Vector2(x,y), isOffset, tilePrefab);
             }
         }
+        this.GetComponent<ObjectPlacer>().PlaceObject();
     }
 
     Tile PickTile(float height) {
