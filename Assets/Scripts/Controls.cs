248c0c99dd854c93ba9d73aeed3c70ff08e477e6
ErikBolumburu <erikbolumburu11@gmail.com>
2022-07-11 21:48:57 +0100

    Working multi select

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
+    [Header("Camera")]
     [SerializeField] float cameraSpeed;
     [SerializeField] float cameraShiftSpeed;
     [SerializeField] float minZoom, maxZoom, zoomSpeed, zoomMoveSpeedEffect;
     World world;
     Camera cam;
+    Vector3 mouseScreenPosition;
+    Vector3 mouseWorldPosition;
+    Vector3 mousePos1;
+    bool dragSelect;
+    [Header("UI")]
+    [SerializeField] GameObject selectionBox;
+
+    [Header("Misc.")]
+    public PlayerState playerState;
 
     void Start(){
         world = GameManager.Instance.world;
@@ -17,10 +28,15 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
+        mouseScreenPosition = Input.mousePosition;
+        mouseWorldPosition = cam.ScreenToWorldPoint(mouseScreenPosition);
+        TransformSelectionBox();
+
         CameraControls();
-        UnitControls();
+        WorldInteraction();
 
         // Misc Keybinds 
+        if(Input.GetKeyDown(KeyCode.Escape)) ChangeToViewingState();
 
         // Temporary Keybinds
         if(Input.GetKeyDown(KeyCode.P)){
@@ -45,16 +61,96 @@ public class Controls : MonoBehaviour
         if(cam.orthographicSize > maxZoom) cam.orthographicSize = maxZoom;
     }
 
-    void UnitControls(){
-        // Right Click To Move Logic Removed For Now
-        /* if(Input.GetMouseButtonDown(1)) {
-            if(GameManager.Instance.unitManager.selectedUnits.Count == 1){
-                Unit cUnit = GameManager.Instance.unitManager.selectedUnits[0];
-                Vector2Int mp = (Vector2Int)Vector3Int.RoundToInt(cam.ScreenToWorldPoint(Input.mousePosition));
-                cUnit.go.GetComponent<UnitGO>().PathToTile(world.tiles[mp.x, mp.y]);
+    void WorldInteraction(){
+        if(Input.GetMouseButtonDown(0)){
+            mousePos1 = mouseScreenPosition;
+        }
+
+        if(Input.GetMouseButton(0)){
+            if((mousePos1 - mouseScreenPosition).magnitude > 40){
+                dragSelect = true;
             }
-            GameManager.Instance.unitManager.selectedUnits = new List<Unit>();
-        } */
+        }
+        
+        if(Input.GetMouseButtonUp(0)){
+            // SINGLE SELECT
+            if(dragSelect == false){
+                switch(playerState){
+                    case PlayerState.VIEWING:
+                        break;
+                    case PlayerState.FORESTING:
+                        if(GameManager.Instance.world.screenToTilePosition().occupyingObject != null){
+                            if(GameManager.Instance.world.screenToTilePosition().occupyingObject.objectType == 0){
+                                GameManager.Instance.world.screenToTilePosition().occupyingObject.markedForHarvest = true;
+                            }
+                        }
+                        break;
+                    case PlayerState.MINING:
+                        if(GameManager.Instance.world.screenToTilePosition().occupyingObject != null){
+                            if(GameManager.Instance.world.screenToTilePosition().occupyingObject.objectType == 1){
+                                GameManager.Instance.world.screenToTilePosition().occupyingObject.markedForHarvest = true;
+                            }
+                        }
+                        break;
+                }
+            }
+            // DRAG SELECT
+            else{
+                Vector2 lowerBound = cam.ScreenToWorldPoint(new Vector3(Mathf.Min(mousePos1.x, mouseScreenPosition.x), Mathf.Min(mousePos1.y, mouseScreenPosition.y)));
+                Vector2 upperBound = cam.ScreenToWorldPoint(new Vector3(Mathf.Max(mousePos1.x, mouseScreenPosition.x), Mathf.Max(mousePos1.y, mouseScreenPosition.y)));
+
+                if(lowerBound.x < 0) lowerBound.x = 0;
+                if(lowerBound.y < 0) lowerBound.y = 0;
+                if(upperBound.x > GameManager.Instance.world.width) upperBound.x = GameManager.Instance.world.width;
+                if(upperBound.y > GameManager.Instance.world.height) upperBound.y = GameManager.Instance.world.height;
+
+                Debug.Log($"lby {Mathf.RoundToInt(lowerBound.y)}, uby{Mathf.RoundToInt(upperBound.y)}");
+
+                for (int x = Mathf.RoundToInt(lowerBound.x); x < Mathf.RoundToInt(upperBound.x); x++)
+                {
+                    for (int y = Mathf.RoundToInt(lowerBound.y); y < Mathf.RoundToInt(upperBound.y); y++){
+                        switch(playerState){
+                            case PlayerState.VIEWING:
+                                break;
+                            case PlayerState.FORESTING:
+                                if(GameManager.Instance.world.tiles[x, y].occupyingObject != null){
+                                    if(GameManager.Instance.world.tiles[x, y].occupyingObject.objectType == 0){
+                                        GameManager.Instance.world.tiles[x, y].occupyingObject.markedForHarvest = true;
+                                    }
+                                }
+                                break;
+                            case PlayerState.MINING:
+                                if(GameManager.Instance.world.tiles[x, y].occupyingObject != null){
+                                    if(GameManager.Instance.world.tiles[x, y].occupyingObject.objectType == 1){
+                                        GameManager.Instance.world.tiles[x, y].occupyingObject.markedForHarvest = true;
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            dragSelect = false;
+        }
     }
 
+    void TransformSelectionBox(){
+        selectionBox.SetActive(dragSelect);
+        RectTransform rect = selectionBox.GetComponent<RectTransform>();
+        float width = mouseScreenPosition.x - mousePos1.x;
+        float height = mouseScreenPosition.y - mousePos1.y;
+        rect.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        rect.anchoredPosition = new Vector2(mousePos1.x, mousePos1.y) + new Vector2(width / 2, height / 2);
+    }
+
+    public void ChangeToViewingState(){ playerState = PlayerState.VIEWING; }
+    public void ChangeToForestingState(){ playerState = PlayerState.FORESTING; }
+    public void ChangeToMiningState(){ playerState = PlayerState.MINING; }
+
+}
+
+public enum PlayerState {
+    VIEWING,
+    FORESTING,
+    MINING,
 }
